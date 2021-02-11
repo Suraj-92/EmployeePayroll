b52969a5c0aa005554f6a52aa1ce99112b0998b4
Suraj-92 <suraj.temkar.92@gmail.com>
2021-02-12 00:12:33 +0530

    Perform File Operations UC-2 Done

--- src/main/java/javapractice/EmployeePayrollService.java
+++ src/main/java/javapractice/EmployeePayrollService.java
@@ -4,10 +4,7 @@ import java.util.*;
 public class EmployeePayrollService {
     private List<EmployeePayrollData> employeePayrollList;
     public EmployeePayrollService() {}
-    public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList)
-    {
-
-    }
+    public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) { }
     public static void main(String[] args)
     {
         ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
